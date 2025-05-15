f9938703b70d26ea2c3f641711baadf16441774a
marcocmaj7 <chitarristamrc@yahoo.it>
2025-05-15 23:16:10 +0200

    testina

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
 print ("ciao")
 print ("ciao")
+
+fwegwef
+WindowsErrorwef
+wef
+wef
+WindowsErrorhtr
+
+rthr
+
+htrhtr
+hasattrh
+trh
+trhrh
+
+hrth
+tryht
+hasattrhr
+hasattrhtr
+hrt
+hasattrtt
+tuple 
