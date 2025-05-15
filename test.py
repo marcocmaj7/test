e32e4134f0aa02e244ddcf4759d7b2feea703e2f
marcocmaj7 <chitarristamrc@yahoo.it>
2025-05-15 23:25:14 +0200

    c

--- test.py
+++ test.py
@@ -1,23 +1,2 @@
 print ("ciao")
 print ("ciao")
-
-fwegwef
-WindowsErrorwef
-wef
-wef
-WindowsErrorhtr
-
-rthr
-
-htrhtr
-hasattrh
-trh
-trhrh
-
-hrth
-tryht
-hasattrhr
-hasattrhtr
-hrt
-hasattrtt
-tuple 
